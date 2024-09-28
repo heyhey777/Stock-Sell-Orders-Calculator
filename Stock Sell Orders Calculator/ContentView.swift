728db949934033ec00bd9dd5e491de355270d21b
zenorbit <ekaterina.tretyakova1@gmail.com>
2024-09-28 14:58:50 +0100

    Basic structure

--- Stock Sell Orders Calculator/ContentView.swift
+++ Stock Sell Orders Calculator/ContentView.swift
@@ -9,14 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ZStack {
+            Rectangle().fill(.purple.opacity(0.1))
+            
+            HStack {
+                VStack {
+                    Image(systemName: "dollarsign.circle.fill")
+                        .imageScale(.large)
+                        .foregroundColor(.orange)
+                    Text("Average price").font(.subheadline)
+                    TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                }
+                
+                VStack {
+                    Image(systemName: "basket.fill")
+                        .imageScale(.large)
+                        .foregroundColor(.orange)
+                    Text("Shares amount").font(.subheadline)
+                    TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                }
+            }
+        }
+        .foregroundColor(.black)
+        
+
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Profit taking").font(.title2)
+            Text("60 shares 25$ for 15% of gains, 30% of the position size")
+            Text("20 shares 45$ for 45% of gains, 10% of the position size")
         }
         .padding()
+        
+        
+        VStack {
+            Divider()
+        }
+        .padding()
+        
+        VStack {
+            Text("Stop loss").font(.title2)
+            Text("15$ for 5% of loss, 20% of the position size")
+        }
+        .padding()
+        
+        VStack {
+            Divider()
+        }
+        .padding()
+        
+        VStack {
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }
+        .padding()
+        
     }
+    
 }
 
 #Preview {
---
+++ Stock Sell Orders Calculator/StockCalculator.swift
@@ -0,0 +1,8 @@
+//
+//  StockCalculator.swift
+//  Stock Sell Orders Calculator
+//
+//  Created by Kate on 28/09/2024.
+//
+
+import Foundation
