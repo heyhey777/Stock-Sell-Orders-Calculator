8f2be6e04a1a4798c23b4863369c08821486c5cd
zenorbit <ekaterina.tretyakova1@gmail.com>
2024-09-28 12:29:12 +0100

    Initial Commit

---
+++ Stock Sell Orders Calculator/Stock_Sell_Orders_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_Sell_Orders_CalculatorApp.swift
+//  Stock Sell Orders Calculator
+//
+//  Created by Kate on 28/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Stock_Sell_Orders_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
